c8ddf50b8125d2098c0a546b9a4f9b2c3b55f939
Kevin Aleman <kevin.aleman@hotmail.com>
2021-06-21 21:41:08 -0500

    se ajusta consulta a dynamo

--- src/dynamoManager.js
+++ src/dynamoManager.js
@@ -72,11 +72,16 @@ module.exports.getAllCitas = () => {
         TableName : process.env.DYNAMOTABLE
     };
 
-	return dynamo
-		.get(params)
-		.promise()
-		.then(item => {
-			return item.Item;
-		});
+	return dynamoquery(params, function(err, data) {
+
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(" -", item.year + ": " + item.title);
+            });
+        }
+    });
 
 };
